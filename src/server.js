761db529018af6017223e8ace7f9334b35f80abd
luis <luiszkm@gmail.com>
2022-09-21 11:23:18 -0300

    att port

--- src/server.js
+++ src/server.js
@@ -34,5 +34,5 @@ app.use((error, req, res, next)=> {
   })
 })
 
-const PORT = process.env.SERVER_PORT || 4444;
+const PORT = process.env.SERVER_PORT || 3333;
 app.listen(PORT, () => console.log(`Estou na porta ${PORT}`));  
