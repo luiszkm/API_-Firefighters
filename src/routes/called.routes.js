64c84acbb92082885db9d8a06261e07ad12e5672
luis <luiszkm@gmail.com>
2022-09-23 16:31:14 -0300

    add route delete

--- src/routes/called.routes.js
+++ src/routes/called.routes.js
@@ -11,6 +11,8 @@ const  callerController = new CalledController()
 calledRoutes.get('/',ensureAuthenticated, callerController.index)
 calledRoutes.post('/',ensureAuthenticated ,callerController.create)
 calledRoutes.get('/:id',ensureAuthenticated, callerController.show)
+
 calledRoutes.put('/:id',admAuthenticated, callerController.update)
+calledRoutes.delete('/:id',admAuthenticated, callerController.delete)
 
 module.exports = calledRoutes
